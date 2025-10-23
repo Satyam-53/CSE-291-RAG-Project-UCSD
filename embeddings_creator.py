41baf039dd8ead184a345fbf26729366cd4754da
Debmalya Pal <debmalyapal.dp@gmail.com>
2025-10-22 21:45:34 -0700

    Added scripts for reading processed data, creating & saving embeddings, and loading them to vectorstore

---
+++ embeddings_creator.py
@@ -0,0 +1,97 @@
+from sentence_transformers import SentenceTransformer
+import os
+import re
+import json
+
+
+def process_all_processed_file(directory_path: str) -> list[str]:
+    all_chunks = []
+    try:
+        # Iterate over all files in the directory
+        for filename in os.listdir(directory_path):
+            if filename.endswith(".txt"):
+                file_path = os.path.join(directory_path, filename)
+                file_content = read_file(file_path)
+                file_chunks = create_chunks(file_content)
+                all_chunks.extend(file_chunks)
+        print("Successfully read data from all processed files.")
+    except:
+        print(f"Error encountered while accessing directory: {directory_name}")
+    finally:
+        return all_chunks
+
+def read_file(file_path: str) -> str:
+    content = ""
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+    except:
+        print(f"Error encountered while reading file: {file_name}")
+    finally:
+        return content
+
+def create_chunks(content: str) -> list[str]:
+    chunks = []
+    try:
+        # Split content into paragraphs (assuming double newline separates paragraphs)
+        paragraphs = content.split('\n')
+        for paragraph in paragraphs:
+            # Strip leading/trailing whitespace
+            paragraph = paragraph.strip()
+            if paragraph:
+                # Split paragraph into sentences using regex
+                sentences = re.split(r'(?<=[.!?])\s+', paragraph)
+                chunks.extend(sentences)
+        print("Created chunks successfully from processed data sources.")
+    except:
+        print("Error encountered while creating chunks.")
+    finally:
+        return chunks
+
+
+def get_embedding_model() -> SentenceTransformer:
+    # Initialize embedding model
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    return model
+
+def create_embeddings(chunks: list[str]) -> list[int | float]:
+    embeddings = []
+    try:
+        model = get_embedding_model()
+        embeddings.extend(model.encode(chunks, show_progress_bar=True).tolist())
+        print("Created embeddings successfully.")
+    except:
+        print("Error encountered while creating embeddings.")
+    finally:
+        return embeddings
+
+def persist_embeddings_to_file(
+    chunks: list[str], 
+    embeddings: list[int | float], 
+    directory_name: str = './embeddings-data/',
+    filename: str = 'embeddings.json'
+) -> None:
+    try:
+        # Save id, chunks (sentences) and corresponding embeddings to a json file
+        data = [
+            {
+                "id": str(i), 
+                "chunk": s, 
+                "embedding": embedding
+            } for i, (s, embedding) in enumerate(zip(chunks, embeddings))
+        ]
+        
+        with open(directory_name+filename, "w", encoding="utf-8") as f:
+            json.dump(data, f)
+        print("Successfully saved embeddings to embeddings.json file.")
+    except:
+        print("Error encountered while saving embeddings to file.")
+
+def main():
+    input_directory = './processed-data/'
+    chunks = process_all_processed_file(input_directory)
+    embeddings = create_embeddings(chunks)
+    persist_embeddings_to_file(chunks, embeddings)
+
+if __name__=='__main__':
+    main()
---
+++ load_embeddings.py
@@ -0,0 +1,55 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import PointStruct
+import json
+import os
+
+def load_data_from_file(directory_name: str = './embeddings-data/', filename: str = 'embeddings.json'):
+    file_path = os.path.join(directory_name, filename)
+    embeddings_data = []
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            embeddings_data.extend(json.load(f))
+        print(f"Read {len(data)} embeddings from {file_path}")
+    except:
+        print(f"Error reading embeddings from file.")
+    finally:
+        return embeddings_data
+
+def get_qdrant_client(host: str = 'loclhost', port: int = 6433, collection_name: str = 'CSE291A-RAG-Project-Phase1') -> QdrantClient:
+    qdrant_client = None
+    try:
+        qdrant_client = QdrantClient(host=host, port=port)
+        # Create collection
+        qdrant_client.recreate_collection(
+            collection_name=collection_name,
+            vectors_config=VectorParams(size=384, distance=Distance.COSINE)
+        )
+        print('Successfully connected to Qdrant client.')
+    except:
+        print('Error connecting to Qdrant client.')
+    finally:
+        return qdrant_client
+
+def persist_chunks_to_qdrant(data: list[dict], collection_name: str = 'CSE291A-RAG-Project-Phase1') -> None:
+    try:
+        qdrant_client = get_qdrant_client()
+    
+        points = [
+            PointStruct(
+                id=datum["id"], 
+                vector=datum["embedding"], 
+                payload={"text": datum["chunk"]} # add other metadata that we might need to store (recency, etc..)
+            ) for datum in data
+        ]
+    
+        qdrant_client.upsert(collection_name=collection_name, points=points)
+        print(f"Stored {len(points)} embeddings in Qdrant vector store.")
+    except:
+        print('Error saving embedding to Qdrant.')
+
+def main():
+    embeddings_list = load_data_from_file()
+    persist_chunks_to_qdrant(embeddings_list)
+
+if __name__=='__main__':
+    main()
